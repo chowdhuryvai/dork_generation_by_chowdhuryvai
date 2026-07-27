0709ca899080981b44701607dc1a365c95396021
Chowdhuryvai <74151069+chowdhuryvai@users.noreply.github.com>
2026-07-27 08:09:44 +0600

    main

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+DorkGen - Google Dork Generator & Search Tool
+DEVELOP BY : CHOWDHURY-VAI
+GITHUB     : https://github.com/chowdhuryvai
+
+Just run: python dork_generation_by_chowdhuryvai.py
+"""
+from dork_generation_by_chowdhuryvai import main
+
+if __name__ == "__main__":
+    main()
